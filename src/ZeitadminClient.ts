5f67240e231bce246f0470da8aaf221156e300d0
Hannes Diercks <github@xiphe.net>
2017-02-06 23:39:30 +0100

    feat(subscribe): add redux store and subscribe method
    in order to use state of the art state management <3
    
    ref https://github.com/Xiphe/zeitadmin-client/issues/3

--- src/ZeitadminClient.ts
+++ src/ZeitadminClient.ts
@@ -2,13 +2,29 @@ import ZeitDatabaseInterface from './interfaces/ZeitDatabaseInterface';
 import ZeitadminClientConfigInterface from './interfaces/ZeitadminClientConfigInterface';
 import parseIssueUrl from './parseIssueUrl';
 import Zeit from './Zeit';
+import { createStore, Store } from 'redux'
+
+interface State {
+  zeit: Array<Zeit>
+}
+
+const initialState: State = {
+  zeit: []
+};
 
 export default class ZeitadminClient {
   db: ZeitDatabaseInterface;
   token: string;
+  store: Store<State>;
   constructor({ db, token }: ZeitadminClientConfigInterface) {
     this.db = db;
     this.token = token;
+    this.store = createStore((state = initialState, action) => {
+      switch (action.type) {
+        default:
+          return state;
+      }
+    });
   }
   start(issueUrl: string, { duration }) {
     return Promise.resolve(issueUrl)
@@ -16,5 +32,10 @@ export default class ZeitadminClient {
       .then((issue) => {
         return new Zeit({ issue, duration }).save(this.db);
       });
+  }
+  subscribe(subscription: (state: State) => void) {
+    return this.store.subscribe(() => {
+      subscription(this.store.getState());
+    });
   }
 }
