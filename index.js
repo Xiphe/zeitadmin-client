087a52689c71bba7c2e59c1030b9b827c1f5120c
Hannes Diercks <github@xiphe.net>
2017-02-08 00:44:12 +0100

    feat(index): export all the things

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('./dist/src/ZeitadminClient').default;
+const expts = require('./dist/src/');
+
+module.exports = expts.default;
+
+Object.keys(expts).forEach((key) => {
+  if (key !== 'default') {
+    module.exports[key] = expts[key];
+  }
+});
